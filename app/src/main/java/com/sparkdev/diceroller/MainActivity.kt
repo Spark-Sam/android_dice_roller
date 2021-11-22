7d07cac0702cc847724177dc50abd7a45ef8a281
Spark-Sam <d.potpop@gmail.com>
2021-11-22 14:37:00 +0300

    The code is: * Refactored. * Commented appropriately. * Added to facilitate a two dice roll.

--- app/src/main/java/com/sparkdev/diceroller/MainActivity.kt
+++ app/src/main/java/com/sparkdev/diceroller/MainActivity.kt
@@ -11,24 +11,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
+        rollButton.setOnClickListener { rollDice() }
+
+        //Do a dice roll when app is Launched
+        rollDice()
+    }
+/**
+* Create a Dice and update screen with the results
+ **/
+    private fun rollDice() {
+    //Create a new dice objects and roll both
+        val myFirstDice = Dice(6)
+        val mySecondDice = Dice(6)
+        val firstDiceRoll = myFirstDice.roll()
+        val secondDiceRoll = mySecondDice.roll()
+
+    //Find image view in the layout
+        val firstDiceImage : ImageView = findViewById(R.id.imageView)
+        val secondDiceImage : ImageView = findViewById(R.id.imageView2)
+
+    fun diceImage(image: Int): Int {
+        //Determine which Drawable resource ID to use based on the number rolled
+        return when (image) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 
-    private fun rollDice() {
-        val myFirstDice = Dice(6)
-        val diceImage : ImageView = findViewById(R.id.imageView)
+    //Update screen with the selected drawable resource ID
+        val firstDisplay = diceImage(firstDiceRoll)
+        firstDiceImage.setImageResource(firstDisplay)
 
-        when(myFirstDice.roll()) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        val secondDisplay = diceImage(secondDiceRoll)
+        secondDiceImage.setImageResource(secondDisplay)
 
-        }
+    //Update the content description
+        firstDiceImage.contentDescription = firstDiceRoll.toString()
+        secondDiceImage.contentDescription = secondDiceRoll.toString()
 
     }
 }
